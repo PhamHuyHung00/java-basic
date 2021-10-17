509cd34dd53139915fd042d5b5fed24c8b2d418d
phamhuyhung <huyhung041000@gmail.com>
2021-10-17 20:49:19 +0700

    bt buoi 3

--- baitap-3/src/lesson3.java
+++ baitap-3/src/lesson3.java
@@ -3,26 +3,40 @@ import java.util.Scanner;
 public class lesson3 {
 
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("Nhập cân nặng của bạn : " );
-        double doubleNumber1 = sc.nextDouble();
-
-        System.out.println("Nhập chiều cao của bạn  : ");
-        double doubleNumber = sc.nextDouble();
-        double bMI = doubleNumber1/(doubleNumber*2);
-
-        System.out.println("Cân nặng của bạn là : "+doubleNumber1 +" kg" );
-        System.out.println("Chiều cao của bạn là : "+doubleNumber +" m");
-        System.out.println("Chỉ số BMI của bạn là :" +bMI);
-        //in ra màn hình chỉ số BMI
-
-
         System.out.println( "Cạnh Huyền của tam giác vuông bằng = "+ math(3, 4));
-        //Sử dụng lớp Mart tính cạnh huyền tam giác vuông !
+
+          parityCheck(3);
+
+         tong(2,4);
+         hieu(2,4);
+         nhan(2,4);
+         thuong(4,4);
+
+
     }
-        public  static double math( double x, double y) {
+        public  static double math( double x, double y){
         return Math.sqrt(x * x + y * y);
+    }
+
+      public static void parityCheck(int a){
+        String s = a % 2 ==0? "a là số chẵn !":"a là số lẻ !";
+          System.out.println(s);
+     }
+
+    public static void tong( int x , int y){
+        System.out.printf("\nTổng của %d và %d bằng : %d ",x, y, x+y);
+    }
+
+    public static void hieu(int x, int y){
+        System.out.printf("\nHiệu của %d và %d bằng: %d",x, y, x-y);
+    }
+
+    public static void nhan(int x, int y){
+        System.out.printf("\nnhân của %d và %d bằng: %d", x, y, x*y);
+    }
+
+    public static void thuong(int x, int y){
+        System.out.printf("\nThương của %d và %d bằng: %d", x, y, x/y);
     }
 
 
