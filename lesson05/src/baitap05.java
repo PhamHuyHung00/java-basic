bf97ddebc5c6c920c22234516806092ef54e7583
phamhuyhung <huyhung041000@gmail.com>
2021-10-22 22:34:14 +0700

    hh

---
+++ lesson05/src/baitap05.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class baitap05 {
+
+    public static void main(String[] args) {
+        lessonOne();
+        lessonTwo("happy new year ", "new");
+        lessonThree();
+
+    }
+
+    static void lessonOne() {
+        String s = "hello every one";
+        int count = 0;
+        for (int i = 0; i < s.length(); i++) {
+            if (s.charAt(i) == 'e')
+                count++;
+        }
+        System.out.println("Số lần xuất hiện chữ e trong chuỗi " + s + " là " + count);
+    }
+
+    static void lessonTwo(String strA, String strB){
+         int result = strA.indexOf(strB);
+        System.out.println("vtri: "+result);
+    }
+
+    static void lessonThree() {
+        String input;
+        input = "khong co gi quy hon doc lap tu do";
+        String[] inputs = input.split(" ");
+        int index = 0;
+        int lenMax =0;
+        for (int i = 0; i < inputs.length; i++) {
+            int len = inputs[i].length();
+            if (len > lenMax){
+                lenMax = len;
+                index = i;
+            }
+        }
+        System.out.println("Tu dai nhat trong chuoi là : "+ inputs[index]);
+        }
+    }
+
+
+
+
