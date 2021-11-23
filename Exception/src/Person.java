4dffada86a1af5389f5905e575ac1cd35d3a336e
phamhuyhung <huyhung041000@gmail.com>
2021-11-23 15:09:40 +0700

    bt

--- Exception/src/Person.java
+++ Exception/src/Person.java
@@ -1,7 +1,12 @@
+import java.time.LocalDate;
+
 public class Person {
+
     public static void person(String name, int year, String address) throws MyException {
-        if (year < 1900 || year > 2021) throw new MyException("Năm sinh không phù hợp");
-        int age = 2021 - year;
+        LocalDate localDate = LocalDate.now();
+        int current_year = localDate.getYear();
+        if (year < 1900 || year > current_year) throw new MyException("Năm sinh không phù hợp");
+        int age = current_year - year;
         System.out.println("Tuổi của bạn là: " + age);
         if (age > 18) {
             System.out.println("Bạn đã trưởng thành, đủ điều kiện bầu cử !!");
