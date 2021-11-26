d0fc7eb45ad70bdc3d1ca88369e271d3d3d0145a
phamhuyhung <huyhung041000@gmail.com>
2021-11-26 16:39:40 +0700

    btfpt

--- FPTShop/src/Main.java
+++ FPTShop/src/Main.java
@@ -13,25 +13,32 @@ public class Main {
                 break;
             case 2:
                 fRepo.printCategory(Category.PHONE);
+                break;
             case 3:
                 fRepo.printCategory(Category.ACCESSORY);
+                break;
             case 4:
                 fRepo.printCategory(Category.Apple);
+                break;
             case 5:
                 fRepo.printProduce();
                 break;
             case 6:
                 System.out.println("Sản phẩm dưới 2tr: ");
                 fRepo.printPrice();
+                break;
             case 7:
                 System.out.println("Sản phẩm từ 2tr - 4tr:");
                 fRepo.printPrice2();
+                break;
             case 8:
                 System.out.println("Sản phẩm từ 4tr - 7tr: ");
                 fRepo.printPrice3();
+                break;
             case 9:
                 System.out.println("Sản phẩm từ 7tr - 13tr: ");
                 fRepo.printPrice4();
+                break;
             case 10:
                 System.out.println("Sản phẩm trên 13tr: ");
                 fRepo.printPrice5();
