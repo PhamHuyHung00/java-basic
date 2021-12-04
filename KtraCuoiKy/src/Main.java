99e1d79775cee6f2cc8d3fccaad79746572cc18a
phamhuyhung <huyhung041000@gmail.com>
2021-12-04 23:43:41 +0700

    SuaBT

--- KtraCuoiKy/src/Main.java
+++ KtraCuoiKy/src/Main.java
@@ -2,7 +2,19 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("1-Đăng nhập    2- Đăng ký");
+        int choose = sc.nextInt();
+        switch (choose) {
+            case 1:
+                Person.login();
+                break;
+            case 2:
+                Person.Register();
+                Person.Menu();
+                break;
+        }
+// gợi ý mật khẩu cô đỡ phải nghĩ 123ph_AM45
 
-        Person.Menu();
     }
 }
--- KtraCuoiKy/src/Person.java
+++ KtraCuoiKy/src/Person.java
@@ -5,23 +5,52 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Person {
-    static String USERNAME = "name";
+    static String USERNAME = "huyhung";
     static String PASSWORD = "1";
-    static String EMAIL = "techmater@gamil.com";
+    static String EMAIL = "hung@gmail.com";
     static Scanner sc = new Scanner(System.in);
+    static String ChangeEmail = "";
+    static String ChangePassword = "";
+    static String ChangeUsername = "";
+    static String registerUsername = "";
+    static String registerEmail = "";
+    static String registerPassword = "";
+    static String inputPassword = "";
+    static String inputEmail = "";
     static List<Information> listInformation = new ArrayList<>();
 
 
-    public static void Menu(){
+    public static void Menu() {
         System.out.println("1-Đăng nhập   2-Đăng ký ");
         int choose = sc.nextInt();
-        switch (choose){
+        switch (choose) {
             case 1:
-                login();
+                login2();
+                Change();
+                break;
+            case 2:
+                Register();
+                break;
 
         }
 
     }
+
+    public static void Menu1() {
+        System.out.println("1-Đăng nhập   2-Đăng ký ");
+        int choose = sc.nextInt();
+        switch (choose) {
+            case 1:
+                inputLogin();
+                break;
+            case 2:
+                Register();
+                break;
+
+        }
+
+    }
+
 
     public static void login() {
         boolean isCheck = false;
@@ -37,43 +66,88 @@ public class Person {
                     String password = sc.nextLine();
                     if (username.equals(USERNAME) && password.equals(PASSWORD)) {
                         System.out.println("Đăng nhập thành công !!!");
+                        System.out.println("Chào mừng " + USERNAME);
                         isCheck = true;
                         Change();
 
+                    } else {
+                        System.out.println("Đăng nhập lại");
                     }
-
-
-//                    if (username != USERNAME) {
-//                        System.out.println("Kiểm tra lại username");
-//                    }
-                    else {
-                        System.out.println("1-Đăng nhập lại !!!");
-                        System.out.println("2-Quên mật khẩu");
+                    if (!username.equals(USERNAME)) {
+                        System.out.println("Kiểm tra lại username");
                     }
-
-
                 }
-
                 break;
             case 2:
                 ForgotPassword();
-
+                login3();
+                break;
 
         }
     }
 
+    public static void Register() {
+        System.out.println("Nhập username: ");
+        registerUsername = sc.nextLine();
+        boolean isCheck = false;
+        while (!isCheck) {
+            try {
+                registerEmail = checkEmail();
+                isCheck = true;
+            } catch (MyException e) {
+                System.out.println(e.getMessage());
+            }
+            try {
+                registerPassword = checkPassword();
+                isCheck = true;
+            } catch (MyException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+        listInformation.add(new Information(registerUsername, registerEmail, registerPassword));
+    }
+
+    public static void login2() {
+        boolean isCheck = false;
+        sc.nextLine();
+        String username;
+        String password;
+        while (!isCheck) {
+            System.out.println("Nhập username: ");
+            username = sc.nextLine();
+
+            System.out.println("Nhập password: ");
+            password = sc.nextLine();
+            if (username.equals(registerUsername) && password.equals(registerPassword)) {
+                System.out.println("Đăng nhập thành công !!!");
+                System.out.println("Chào mừng " + registerUsername);
+                isCheck = true;
+
+
+            } else {
+                System.out.println("Đăng nhập lại");
+            }
+            if (!username.equals(registerUsername)) {
+                System.out.println("Kiểm tra lại username");
+            }
+        }
+    }
+
+
     public static void ForgotPassword() {
         boolean isCheck = false;
         System.out.println("Nhập email của bạn: ");
-        String inputEmail = sc.nextLine();
-
+        inputEmail = sc.nextLine();
         if (inputEmail.equals(EMAIL)) {
-            System.out.println("Nhập mật khẩu cần đổi: ");
-            String inputPassword = sc.nextLine();
+//            System.out.println("Nhập mật khẩu cần đổi: ");
+//            inputPassword = sc.nextLine();
             while (!isCheck) {
                 try {
                     inputPassword = checkPassword();
+
                     isCheck = true;
+                    System.out.println("Đổi mật khẩu thành công !!");
+
                     listInformation.add(new Information(USERNAME, EMAIL, inputPassword));
                 } catch (MyException e) {
                     System.out.println(e.getMessage());
@@ -81,19 +155,43 @@ public class Person {
             }
 
         } else System.out.println("Tài khoản không tồn tại");
+
     }
 
-    public static void Change() {
-        System.out.println("1 - Thay đổi username" + " 2 - Thay đổi email" + " 3 - Thay đổi mật khẩu" + " 4 - Đăng xuất " + " 0 - Thoát chương trình");
+    public static void login3() {
+        System.out.println("Đăng nhập tài khoản:");
         boolean isCheck = false;
-        String ChangeEmail = "";
-        String ChangePassword = "";
+        while (!isCheck) {
+            System.out.println("Nhập username: ");
+            sc.nextLine();
+            System.out.println("Nhập password: ");
+            String password = sc.nextLine();
+            if (password.equals(inputPassword)) {
+                System.out.println("Đăng nhập thành công !!!");
+                System.out.println("Chào mừng " + USERNAME);
+                isCheck = true;
+
+
+            } else {
+                System.out.println("Đăng nhập lại");
+            }
+            if (!password.equals(inputPassword)) {
+                System.out.println("Kiểm tra lại password");
+            }
+        }
+    }
+
+
+    public static void Change() {
+        System.out.println("1 - Thay đổi username-email-password" + "  2- Đăng xuất " + " 0 - Thoát chương trình");
+        boolean isCheck = false;
+
         int choose = sc.nextInt();
+        sc.nextLine();
         switch (choose) {
             case 1:
-                System.out.println("Nhập username muốn thay đổi: ");
-                String ChangeUsername = sc.nextLine();
-                System.out.println("Nhập email muốn thay đổi: ");
+                System.out.println("Nhập username : ");
+                ChangeUsername = sc.nextLine();
                 while (!isCheck) {
                     try {
                         ChangeEmail = checkEmail();
@@ -101,32 +199,60 @@ public class Person {
                     } catch (MyException e) {
                         System.out.println(e.getMessage());
                     }
-                }
-                System.out.println("Nhập mật khẩu muốn thay đổi: ");
-                while (!isCheck) {
+
                     try {
                         ChangePassword = checkPassword();
                         isCheck = true;
                     } catch (MyException e) {
                         System.out.println(e.getMessage());
                     }
+                    listInformation.add(new Information(ChangeUsername, ChangeEmail, ChangePassword));
                 }
-                listInformation.add(new Information(ChangeUsername, ChangeEmail, ChangePassword));
+                Change();
                 break;
             case 2:
-                login();
+                Menu1();
+                Change();
                 break;
             case 0:
+                System.out.println("^_^ Bye Bye ^_^");
                 System.exit(0);
+                break;
+            default:
+                System.out.println("Không có lựa chọn này !!!");
         }
 
 
     }
 
+    public static void inputLogin() {
+        boolean isCheck = false;
+        sc.nextLine();
+        while (!isCheck) {
+            System.out.println("Nhập username: ");
+            String username = sc.nextLine();
+            System.out.println("Nhập password: ");
+            String password = sc.nextLine();
+            if (username.equals(ChangeUsername) && password.equals(ChangePassword)) {
+                System.out.println("Đăng nhập thành công !!!");
+                System.out.println("Chào mừng " + ChangeUsername);
+                isCheck = true;
 
-    public void ChangeAccount() {
+
+            } else {
+                System.out.println("Đăng nhập lại");
+            }
+            if (!username.equals(ChangeUsername)) {
+                System.out.println("Kiểm tra lại username");
+            }
+        }
+    }
+
+
+    public static void ChangeAccount() {
         System.out.println("Nhập username: ");
         String username = sc.nextLine();
+        sc.nextLine();
         boolean isCheck = false;
         String email = "";
         String password = "";
@@ -137,8 +263,6 @@ public class Person {
             } catch (MyException e) {
                 System.out.println(e.getMessage());
             }
-        }
-        while (!isCheck) {
             try {
                 password = checkPassword();
                 isCheck = true;
@@ -151,8 +275,9 @@ public class Person {
 
     }
 
+
     public static String checkEmail() throws MyException {
-        System.out.println("Nhập eamil: ");
+        System.out.println("Nhập email: ");
         String email = sc.nextLine();
         String regex = "^[a-zA-Z]+[a-z0-9]*@{1}\\w+mail.com$";
         if (!Pattern.matches(regex, email)) throw new MyException("Email không hợp lệ !!!");
@@ -164,8 +289,8 @@ public class Person {
     public static String checkPassword() throws MyException {
         System.out.println("nhập password: ");
         String password = sc.nextLine();
-        String rexgex = "^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[.,-_;])(?=\\\\S+$).{7,15}$";
-        if (!Pattern.matches(rexgex, password)) throw new MyException("Password không hợp lệ !!!");
+        String regex = "(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[.,-_;])(?=\\S+$).{7,15}";
+        if (!Pattern.matches(regex, password)) throw new MyException("Password không hợp lệ !!!");
         else {
             return password;
         }
